f1947755923a9b1efe1f92ad9a30076d139f6828
Mukhammadali <ali_6017@mail.ru>
2022-08-13 13:33:41 +0500

    Add  AboutSection and intro package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+// import pages
+import AboutUs from "./pages/AboutUs";
+
 function App() {
   return (
     <div className="App">
       <h1>e-Learnihg</h1>
+      <AboutUs />
     </div>
   );
 }
---
+++ src/components/AboutSection.js
@@ -0,0 +1,32 @@
+import React from "react";
+// import images
+import homeIntro from "../img/Biznes.jpg";
+const AboutSection = () => {
+  return (
+    <div className="about">
+      <div className="description">
+        <div className="title">
+          <div className="hide">
+            <h2>Be educated so that</h2>
+          </div>
+          <div className="hide">
+            <h2> you can change</h2>
+          </div>
+          <div className="hide">
+            <h2>the world</h2>
+          </div>
+        </div>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum,
+          rerum.
+        </p>
+        <button>About us</button>
+      </div>
+      <div className="image">
+        <img src={homeIntro} alt="developer groups" width="350px" />
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
