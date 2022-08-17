6d52ab6ee9bb3237a4dc93d9c8c00cf935b61026
Mukhammadali <ali_6017@mail.ru>
2022-08-17 13:18:51 +0500

    Add FaqSection commit motto

---
+++ src/components/FaqSection.js
@@ -0,0 +1,33 @@
+import React, {createRef} from "react";
+
+const FaqSection = () => {
+    return <div className="faq">
+        <h2>Any Questions <span>FAQ</span></h2>
+        <div className="question">
+            <h4>Могу ли я научиться программированию с вами?</h4>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dolor explicabo facere magni quo recusandae.</p>
+            </div>
+        </div>
+        <div className="question">
+            <h4>Как оплачивается стоимость курсов?</h4>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dolor explicabo facere magni quo recusandae.</p>
+            </div>
+        </div>
+        <div className="question">
+            <h4>После покупки этого курса, смогу ли я учиться вместе с братом?</h4>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dolor explicabo facere magni quo recusandae.</p>
+            </div>
+        </div>
+        <div className="question">
+            <h4>Могу ли я научиться программированию с вами?</h4>
+            <div className="answer">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dolor explicabo facere magni quo recusandae.</p>
+            </div>
+        </div>
+    </div>
+}
+
+export default FaqSection;
